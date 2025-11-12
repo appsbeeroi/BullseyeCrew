2be6fe9a675362d315940ca7f226233552e68d4a
esplusnin <esplusnin94@yandex.ru>
2025-11-12 10:49:57 +0300

    Initial Commit

---
+++ BullseyeCrew/BullseyeCrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeCrewApp.swift
+//  BullseyeCrew
+//
+//  Created by Евгений on 12.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeCrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
